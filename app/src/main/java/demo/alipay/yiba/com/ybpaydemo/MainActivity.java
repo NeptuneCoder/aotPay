3d3b5aecabf5f8f4f580e12bbd4f3e9ded5fa697
yanghai23 <GitHub163@126.com>
2017-10-30 12:52:10 +0800

    添加一个手动填写支付信息

--- app/src/main/java/demo/alipay/yiba/com/ybpaydemo/MainActivity.java
+++ app/src/main/java/demo/alipay/yiba/com/ybpaydemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.yiba.pay.IAliOrderInfo;
@@ -20,12 +21,16 @@ public class MainActivity extends AppCompatActivity implements IAliOrderInfo,IWx
     public static final String APPID = "2017102309473363";
     public static final String RSA2_PRIVATE = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC1Asmm936+SnW5lHWLn3bfzN+ZyGlQ7OiDJ6EC7vMd3qXoYKzuJTiU6bU2/LNqfUgF4chHjH8iZ6nXwLYm/iGk5Ynigr+3B31UeFoAS/8MoubBLHblOUVn5cDlGiXktE+/Jwns4ACCd9Q8cpGPtBomnlgLC5M59MYM6KSv8O6nd18KIFQb4/GG6mGujKIhe7qFUHcRkG3DVU1lSqTQh8Z7PbCH0q9ht361Y0tm1rFcgjHjJ6YLL3elBb5wNz8ZaMZbhna43SB7ecMATsCOFLPhyPVK9OrhPBKTlqdJro1ITO0bgjxkxyiHjiPcTQuAzd8AJRqWjzxPa9ZXz44Rr63BAgMBAAECggEAXZ9Qf3yZYIEQvOjtl0bmbEECEf9XgZTXeibAH81Jj9R+CWAfcZls15C9uvAfOGAOJYSvW1pu03O8pCw3ypk8+5YBqFeqI1fLWipZmrez9mw3YAjIgi0KGf/6skTA/rbIXH0rpAoEGXczHgTABwzHTdwue6AECUDyQvD9ZUb/mTvE3XtZaX0CJKa2wJFgMDMZks0hVya78i6JmRqdJuTZiHssPRJj4Yj+olR2jQNatnLAi56b3ftNR4rTT37cA3qnltIPqcpYhVq9pm6OnYZQ1C5WXH+3+82/f3rY4BhJdxIH+91+Qwofvfi1reGo8qJv8YCQBr5vmlRFpsg3WRbDAQKBgQDgPPVa6Nl+Sz3D7vLTh/km/AOB086JgiGclZn+I/Oix2ci0dQddkCo6ldA/VocRQWPb6l2QDngR7VpXd12sn3pOp+dQ/JozRl0ZtFbbUvlhEVBDqa8cuhg5f4Xs0sI7PcL6sNdlhhsa4IL2VbKwHMTGeZKAb6V3t0VUsiefpx9EQKBgQDOpmCvphq2hs44ib6Uusp0A4lj7Oy2XhvCVdb8DIfPlxLaaP56z3I2O8OlM0EeIxlIw890BfbQEXr/DmRcF5Fp97TnCW6+inh7XFZdS7Rf3xxLQozv8HpC5j45ql0CoN2b6A8JdWtLZBHtQtoL4hSOScu8jOGzWxo+tFbkFc3lsQKBgARzkVdVqgOk0LwUAUQLvfl1JUitgLsgFzS5j44I/qwrkzOQwMo4772qsUK8BM7s29hmGhIb4ko+gi5uS5gwoOlBXavMjoJgF/JEnEFZYRlUT9+jv9Gb4lsSFcI4r/OkaJ8W8Pvpn+B/HdzIWr25pk8pw3WSpUrqWq8yroLe9pJRAoGBAKQoXPQLknreuaEGIyQJAlyIlrKPejcgqRKGmCJfXd+VVj+0Wjt64XKdpTxta9Qh86rvCbRULI165WcJqsXRXw173fQOdS/d1fnBD0ZLpjzmgpZyhnzwXl8YgePTJtG4snJwNMnBwfsAwpDko0v/S28AxlzA2WZd/4pCPigv/gDxAoGAFe4jkBcWQZSL/Vg4be9pfxUDBz34eM9NfYpDZjIls8WWghppsDl4933SwZrkzydZ4qeBEVHxOCCrfYGIjnZKta/laFfD1K6S0dcaB7NuoWATAjoxZtsNTo0Iegfi5ruC+GGlV8mJ0bhoPW6ljmAmQijLseNGoXTwK/h/to3ANGo=";
     public static final String RSA_PRIVATE = "";
+    private EditText OrderInfoEt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         YiBaPayConfig.setContext(this);
         final Button showPay = findViewById(R.id.showPay);
+
+
+        OrderInfoEt = findViewById(R.id.et);
         YiBaPayManager.getInstance().setAliOrderInfo(this);
         YiBaPayManager.getInstance().setWxOrderInfo(this);
 
@@ -34,7 +39,13 @@ public class MainActivity extends AppCompatActivity implements IAliOrderInfo,IWx
         showPay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                YiBaPayManager.getInstance().alipay();
+
+                if (!OrderInfoEt.getText().toString().trim().isEmpty()){
+                    YiBaPayManager.getInstance().alipay();
+                }else{
+                    Toast.makeText(MainActivity.this,"订单信息不能为空",0).show();
+                }
+
             }
         });
         YiBaPayManager.getInstance().setResultListener(new IResultListener() {
@@ -66,7 +77,7 @@ public class MainActivity extends AppCompatActivity implements IAliOrderInfo,IWx
         String privateKey = rsa2 ? RSA2_PRIVATE : RSA_PRIVATE;
         String sign = OrderInfoUtil2_0.getSign(params, privateKey, rsa2);
         final String orderInfo = orderParam + "&" + sign;
-        return "alipay_sdk=alipay-sdk-java-dynamicVersionNo&app_id=2017102309473363&biz_content=%7B%22body%22%3A%22%E6%88%91%E6%98%AF%E6%B5%8B%E8%AF%95%E6%95%B0%E6%8D%AE%22%2C%22out_trade_no%22%3A%222017102622372850346%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22subject%22%3A%22%E6%97%A0%E7%BA%A7%E5%88%AB%E5%88%91%E5%A4%A9%E4%B9%8B%E9%80%86%22%2C%22timeout_express%22%3A%2230m%22%2C%22total_amount%22%3A%220.01%22%7D&charset=UTF-8&format=json&method=alipay.trade.app.pay&notify_url=http%3A%2F%2Fad.18wifibank.com%2FS32DD%2Falipay%2Fnotify&sign=da6Kvq76xHSPqS4oV26nSjUEXuh%2FsPXddaRZjrxjnCQ6gMmKSvElxhL9w94BLFeDE3hj%2FEbQlF%2B4TzhqafPyA%2BE2XEFIbQwK9O3IcWESgYF81KyKF9TrHkPmmd9Uhy3%2BQyNpLSc1Kbt5oICvSbkXIoCbZLsW%2BkZqrpDODRfebX9QhqdIZCdE7oDnLKmnMH4DVMoalkGSzy3dWfHRRiI6oCHMe0KwT1eu0FkAgJohcPHp%2FxZYAE%2Bccta1m43gF9OZxztnlnR2olQY60Qw2JQi%2BD6blbjPkcuAcJdbpL6GuRIqcXvQs10fvOJhaDkqTybvOlalpeQOwzIlAfMTpoYFsg%3D%3D&sign_type=RSA2&timestamp=2017-10-27+10%3A37%3A28&version=1.0";
+        return OrderInfoEt.getText().toString().trim();
     }
 
     @Override
